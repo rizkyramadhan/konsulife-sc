5997fb5977ee0514a1e2ba8e6e63309fe6aec5c0
Teguh Satriya <teguh.satriya.w@gmail.com>
2019-06-24 21:35:36 +0700

    fixing pr

--- src/page/pr/ListPR.tsx
+++ src/page/pr/ListPR.tsx
@@ -1,37 +1,37 @@
 import { APISearch, APISearchProps } from '@app/api';
-import IconAdd from "@app/libs/ui/Icons/IconAdd";
-import { isSize } from "@app/libs/ui/MediaQuery";
+// import IconAdd from "@app/libs/ui/Icons/IconAdd";
+// import { isSize } from "@app/libs/ui/MediaQuery";
 import UIBody from "@app/libs/ui/UIBody";
-import UIButton from "@app/libs/ui/UIButton";
+// import UIButton from "@app/libs/ui/UIButton";
 import UIContainer from "@app/libs/ui/UIContainer";
 import UIHeader from "@app/libs/ui/UIHeader";
 import UIList from "@app/libs/ui/UIList";
-import UIText from "@app/libs/ui/UIText";
+// import UIText from "@app/libs/ui/UIText";
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router";
 
-const BtnCreate = withRouter(({ history }: any) => {
-  return (
-    <UIButton
-      size="small"
-      color="primary"
-      onPress={() => {
-        history.push("/pr/form");
-      }}
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-end"
-      }}
-    >
-      <IconAdd color="#fff" />
-      {isSize(["md", "lg"]) && (
-        <UIText style={{ color: "#fff" }}>Create</UIText>
-      )}
-    </UIButton>
-  );
-});
+// const BtnCreate = withRouter(({ history }: any) => {
+//   return (
+//     <UIButton
+//       size="small"
+//       color="primary"
+//       onPress={() => {
+//         history.push("/pr/form");
+//       }}
+//       style={{
+//         display: "flex",
+//         flexDirection: "row",
+//         justifyContent: "flex-end"
+//       }}
+//     >
+//       <IconAdd color="#fff" />
+//       {isSize(["md", "lg"]) && (
+//         <UIText style={{ color: "#fff" }}>Create</UIText>
+//       )}
+//     </UIButton>
+//   );
+// });
 
 export default withRouter(observer(({ history, showSidebar, sidebar }: any) => {
   const [data, setData] = useState([]);
@@ -53,7 +53,6 @@ export default withRouter(observer(({ history, showSidebar, sidebar }: any) => {
   return (
     <UIContainer>
       <UIHeader showSidebar={showSidebar} sidebar={sidebar} center={"Purchase Receipt"}>
-        <BtnCreate />
       </UIHeader>
       <UIBody>
         <UIList
