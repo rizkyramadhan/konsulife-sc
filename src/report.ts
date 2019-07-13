9df0bd7f9aebbc35034014ae9fe5daecaf27a031
Nizam Miftahul <nizamiftahul@gmail.com>
2019-07-13 08:42:27 +0700

    merge

--- src/report.ts
+++ src/report.ts
@@ -1,20 +1,26 @@
-import Axios from 'axios';
-import config from './config';
-import {saveAs} from 'file-saver';
+import Axios from "axios";
+import config from "./config";
+import { saveAs } from "file-saver";
 
-export const ReportPost = (type:string,data: any) => {
-    return new Promise((reject) => {
-      Axios.post(config.wsBackend + "api/" + type, JSON.stringify(data), {
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            responseType : 'arraybuffer',
-          }
-        }).then((response) => {
-          let blob = new Blob([response.data], {type: "application/pdf;charset=utf-8"});
-          saveAs(blob,"tes.pdf");
-        }).catch((error) => {
-          console.log(error);
+export const ReportPost = (type: string, data: any) => {
+  return new Promise((resolve, reject) => {
+    Axios.post(config.wsBackend + "api/" + type, JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        responseType: "arraybuffer"
+      }
+    })
+      .then(response => {
+        let blob = new Blob([response.data], {
+          type: "application/pdf;charset=utf-8"
         });
-    });
-  };
+        saveAs(blob, "tes.pdf");
+        resolve();
+      })
+      .catch(error => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
